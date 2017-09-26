525ea6c97c6156e82731e1660ea81492de27a5fa
SabbanIndustrial <sabbanindustrial@gmail.com>
2017-09-26 16:08:45 +0300

    Добавьте файлы проекта.

---
+++ WinLocker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WinLocker
+{
+   static class Program
+   {
+      /// <summary>
+      /// Главная точка входа для приложения.
+      /// </summary>
+      [STAThread]
+      static void Main()
+      {
+         Application.EnableVisualStyles();
+         Application.SetCompatibleTextRenderingDefault( false );
+         Application.Run( new Form1() );
+      }
+   }
+}
